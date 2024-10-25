b4609b27cf1c3b11c8910af62e63de9ed25657e5
nicole <nicole.rezende2008@gmail.com>
2024-10-25 15:29:43 -0300

    Add files via upload

---
+++ atvd extra/extra.js
@@ -0,0 +1,9 @@
+let soma, subtracao, multiplicacao, divisao
+
+function guardarNotas(n1,n2){
+
+    var n1 = parseInt(prompt("Digite o primeiro valor da soma"))
+    var n2 = parseInt(prompt("Digite o segundo valor da soma"))
+
+}
+
